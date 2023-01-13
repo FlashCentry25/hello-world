4eaee09e4c8e0aeeb41c86887d792ecedb3b3783
FlashCentry25 <122560073+FlashCentry25@users.noreply.github.com>
2023-01-13 13:30:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Angry)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("I will be dead by daylight")
+    basic.showIcon(IconNames.Sad)
+})
+basic.showIcon(IconNames.Yes)
